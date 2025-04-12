e31d156e5f7d2e0650f03179723fed4ac1bc6bfe
ArvindVeda <arvind.p21059@iimtrichy.ac.in>
2025-04-12 15:24:40 -0500

    ff

--- src/main/java/com/example/dinerestaurant/DinerestaurantApplication.java
+++ src/main/java/com/example/dinerestaurant/DinerestaurantApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DinerestaurantApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DinerestaurantApplication.class, args);
 	}
-
 }
