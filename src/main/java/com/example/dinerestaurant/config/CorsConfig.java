3703a3a87fc6ed9f2ec25bf2df4cda62133645e5
ArvindVeda <arvind.p21059@iimtrichy.ac.in>
2025-04-14 22:46:04 -0500

    cors

--- src/main/java/com/example/dinerestaurant/config/CorsConfig.java
+++ src/main/java/com/example/dinerestaurant/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://dinerestaurant.onrender.com")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
