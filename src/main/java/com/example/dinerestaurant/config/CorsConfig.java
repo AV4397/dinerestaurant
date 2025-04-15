00f7a8772a18191fff0af0a6dc094ea9621dffcf
ArvindVeda <arvind.p21059@iimtrichy.ac.in>
2025-04-14 19:23:44 -0500

    controller menu 14th april

--- src/main/java/com/example/dinerestaurant/config/CorsConfig.java
+++ src/main/java/com/example/dinerestaurant/config/CorsConfig.java
@@ -2,22 +2,16 @@ package com.example.dinerestaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://dinerestaurant.onrender.com")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://dinerestaurant.onrender.com")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
